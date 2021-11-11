fa62076ba2ff19d652f1b28e01e29331696aad41
Angie Catalina Campos Perilla <ac.campos@uniandes.edu.co>
2021-11-10 23:28:21 -0500

    .

--- App/controller.py
+++ App/controller.py
@@ -61,8 +61,8 @@ def indexSize(analyzer):
     return model.indexSize(analyzer)
 
 def countAvistabyHour(analyzer,hourInicial,hourFinal):
-    hour1 = datetime.time.isoformat(hourInicial)
-    hour2 = datetime.time.isoformat(hourFinal)
+    hour1 = datetime.time.fromisoformat(hourInicial)
+    hour2 = datetime.time.fromisoformat(hourFinal)
     return model.countAvistabyHour(analyzer,hour1,hour2)
 
 def countAvista(analyzer,fechaInicial,fechaFinal) :
